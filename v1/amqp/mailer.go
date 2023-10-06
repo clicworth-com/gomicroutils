e80eaad579f1b64db2da6cc71cbcbf221d58d22a
cb-adhiman <87748037+cb-adhiman@users.noreply.github.com>
2023-10-06 11:03:36 +0530

    Update mailer.go

--- v1/amqp/mailer.go
+++ v1/amqp/mailer.go
@@ -14,14 +14,13 @@ import (
 )
 
 type MailRequest struct {
-	From         string `json:"from"`
-	To           string `json:"to"`
-	Subject      string `json:"subject"`
-	Message      string `json:"message"`
-	MessageType  string `json:"message_type"`
-	TemplateName string `json:"template_name"`
-	AckRequired  bool   `json:"ack_required"`
-	Priority     string `json:"priority"`
+	From         string            `json:"from"`
+	To           string            `json:"to"`
+	Subject      string            `json:"subject"`
+	DataMap      map[string]string `json:"data_map"`
+	TemplateName string            `json:"template_name"`
+	AckRequired  bool              `json:"ack_required"`
+	Priority     string            `json:"priority"`
 }
 
 type MailResponse struct {
