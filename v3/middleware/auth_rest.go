61eb43fcf6137f37e1cd17ca75475210915e46f5
cb-adhiman <87748037+cb-adhiman@users.noreply.github.com>
2023-10-18 16:24:59 +0530

    Update auth_rest.go

--- v3/middleware/auth_rest.go
+++ v3/middleware/auth_rest.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"context"
+	"encoding/json"
 	"net/http"
 	"strings"
 
@@ -11,33 +12,39 @@ import (
 
 func RequestAuth(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		m := make(map[string]string)
-		authorised := "false"
-		m["authorised"] = authorised
-		authHeader := strings.Split(r.Header.Get("Authorization"), "Bearer ")
-		if len(authHeader) == 2 {
-			jwtToken := authHeader[1]
-			claims, err := grpcclient.GetAuthClient().Verify(jwtToken, mux.CurrentRoute(r).GetName())
-			if err != nil {
-				ctx := context.WithValue(r.Context(), "claims", m)
-				next.ServeHTTP(w, r.WithContext(ctx))
-				return
-			}
-			if claims.Authorised {
-				authorised = "true"
-			}
-			m["authorised"] = authorised
-			m["name"] = claims.Name
-			m["emailid"] = claims.EmailId
-			m["phone"] = claims.PhoneNumber
-			m["role"] = claims.Role
-
-			ctx := context.WithValue(r.Context(), "claims", m)
-
-			next.ServeHTTP(w, r.WithContext(ctx))
-		} else {
-			ctx := context.WithValue(r.Context(), "claims", m)
-			next.ServeHTTP(w, r.WithContext(ctx))
+		ai := grpcclient.AuthInfo{
+			Authorised:  false,
+			Name:        "",
+			EmailId:     "",
+			PhoneNumber: "",
+			Role:        "",
 		}
+		byteArray, _ := json.Marshal(ai)
+
+		authHeader := strings.Split(r.Header.Get("Authorization"), "Bearer ")
+		if len(authHeader) != 2 {
+			ctx := context.WithValue(r.Context(), "claims", string(byteArray))
+			next.ServeHTTP(w, r.WithContext(ctx))
+			return
+		}
+		jwtToken := authHeader[1]
+		claims, err := grpcclient.GetAuthClient().Verify(jwtToken, mux.CurrentRoute(r).GetName())
+		if err != nil {
+			ctx := context.WithValue(r.Context(), "claims", string(byteArray))
+			next.ServeHTTP(w, r.WithContext(ctx))
+			return
+		}
+
+		ai.Authorised = claims.Authorised
+		ai.Name = claims.Name
+		ai.EmailId = claims.EmailId
+		ai.PhoneNumber = claims.PhoneNumber
+		ai.Role = claims.Role
+
+		byteArray, _ = json.Marshal(ai)
+
+		ctx := context.WithValue(r.Context(), "claims", string(byteArray))
+
+		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
